295f40bdf397af98fabf8df443029111ecc0b8d6
Igor <42179042+IgorPRZ@users.noreply.github.com>
2022-07-13 20:30:33 -0300

    Add first code

---
+++ IACamera/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace IACamera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CameraBox = new Emgu.CV.UI.ImageBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.btn_salvarFace = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.CameraBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CameraBox
+            // 
+            this.CameraBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.CameraBox.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
+            this.CameraBox.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            this.CameraBox.Location = new System.Drawing.Point(0, 0);
+            this.CameraBox.Name = "CameraBox";
+            this.CameraBox.Size = new System.Drawing.Size(682, 411);
+            this.CameraBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CameraBox.TabIndex = 2;
+            this.CameraBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(688, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name:";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_name.Location = new System.Drawing.Point(688, 320);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(177, 33);
+            this.txt_name.TabIndex = 5;
+            this.txt_name.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txt_nome_KeyUp);
+            // 
+            // btn_salvarFace
+            // 
+            this.btn_salvarFace.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_salvarFace.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salvarFace.Location = new System.Drawing.Point(688, 362);
+            this.btn_salvarFace.Name = "btn_salvarFace";
+            this.btn_salvarFace.Size = new System.Drawing.Size(177, 37);
+            this.btn_salvarFace.TabIndex = 6;
+            this.btn_salvarFace.Text = "Save Face";
+            this.btn_salvarFace.UseVisualStyleBackColor = true;
+            this.btn_salvarFace.Click += new System.EventHandler(this.btnSaveCLick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 411);
+            this.Controls.Add(this.btn_salvarFace);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CameraBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.CameraBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox CameraBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Button btn_salvarFace;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ IACamera/Form1.cs
@@ -0,0 +1,254 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IACamera
+{
+    public partial class Form1 : Form
+    {
+        MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.6d, 0.6d);
+        HaarCascade faceDetected;
+        Image<Bgr, byte> frame;
+        Capture camera;
+        Image<Gray, byte> result;
+        Image<Gray, byte> TrainedFace = null;
+        Image<Gray, byte> grayFace = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> Users = new List<string>();
+        int Count, NumLabels, t;
+        string name, names = null;
+
+        private bool mouseDown;
+        private Point lastLocation;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            faceDetected = new HaarCascade("haarcascade_frontalface_default.xml");
+
+            try
+            {
+                Start();
+                string Labelsinf = "";
+                if (File.Exists(Application.StartupPath + "/Faces/Faces.txt")) 
+                {
+                    Labelsinf = File.ReadAllText(Application.StartupPath + "/Faces/Faces.txt");
+                }
+              
+                string[] Labels = Labelsinf.Split(',');
+                NumLabels = Convert.ToInt16(Labels[0]);
+                Count = NumLabels;
+
+                string FacesLoad;
+                for (int i = 1; i <= NumLabels; i++)
+                {
+                    FacesLoad = "face" + i + ".bmp";
+                    //trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/Faces/Faces.txt"));
+                    trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/Faces/" + FacesLoad);
+                    labels.Add(Labels[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nothing in the Database");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Configure() 
+        {
+            try
+            {
+                int negative_counter = 1;
+                int positive_counter = 1;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+
+
+                    foreach (var item in openFileDialog1.FileNames)
+                    {
+                        string path = item.Substring(0, item.LastIndexOf("\\"));
+                        System.IO.File.Move(item, string.Format("{0}\\negativo{1}.jpg", path, negative_counter));
+
+                        negative_counter++;
+                    }
+                }
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+
+                    foreach (var item in openFileDialog1.FileNames)
+                    {
+                        string path = item.Substring(0, item.LastIndexOf("\\"));
+                        System.IO.File.Move(item, string.Format("{0}\\positivo{1}.jpg", path, positive_counter));
+
+                        positive_counter++;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void txt_nome_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyValue == 13)
+                {
+                    btn_salvarFace.PerformClick();
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                mouseDown = true;
+                lastLocation = e.Location;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                mouseDown = false;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (mouseDown)
+                {
+                    this.Location = new Point(
+                        (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                    this.Update();
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void Start() 
+        {
+            try
+            {
+                camera = new Capture(1);
+                camera.QueryFrame();
+                Application.Idle += Application_Idle;
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            try
+            {
+                Users.Add("");
+                frame = camera.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                grayFace = frame.Convert<Gray, byte>();
+                MCvAvgComp[][] facesDetectedNow = grayFace.DetectHaarCascade(faceDetected, 1.2,10,Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20,20));
+
+                foreach (MCvAvgComp f in facesDetectedNow[0])
+                {
+                    result = frame.Copy(f.rect).Convert<Gray, Byte>().Resize(100,100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                    frame.Draw(f.rect, new Bgr(Color.Green), 2);
+
+                    if (trainingImages.ToArray().Length != 0 )
+                    {
+                        MCvTermCriteria termCriteria = new MCvTermCriteria(Count, 0.001);
+                        EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 1500, ref termCriteria);
+                        name = recognizer.Recognize(result);
+                        frame.Draw(name, ref font, new Point(f.rect.X - 1 , f.rect.Y -1), new Bgr(Color.Red));
+                        Console.WriteLine(name);
+                    }
+
+                    //Users[t - 1] = name;
+                    Users.Add("");
+                }
+
+                CameraBox.Image = frame;
+                names = "";
+                Users.Clear();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void btnSaveCLick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txt_name.Text)) 
+                {
+                    return;
+                }
+                Count = Count + 1;
+                grayFace = camera.QueryGrayFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                MCvAvgComp[][] detectionFaces = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+                foreach (MCvAvgComp f in detectionFaces[0])
+                {
+                    TrainedFace = frame.Copy(f.rect).Convert<Gray, byte>();
+                     break;
+                }
+                if (TrainedFace != null)
+                {
+                    TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                    trainingImages.Add(TrainedFace);
+                    labels.Add(txt_name.Text);
+                    File.WriteAllText(Application.StartupPath + "/Faces/Faces.txt", trainingImages.ToArray().Length.ToString() + ",");
+                    
+                    for (int i = 1; i < trainingImages.ToArray().Length + 1; i++)
+                    {
+                        trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/Faces/face" + i + ".bmp");
+                        File.AppendAllText(Application.StartupPath + "/Faces/Faces.txt", labels.ToArray()[i - 1] + ",");
+                    }
+                    
+                    MessageBox.Show($"{txt_name.Text} Adicionado com sucesso.");
+                }
+                else 
+                {
+                    MessageBox.Show("Nenhum rosto detectado.");
+                }
+
+                TrainedFace = null;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+    }
+}
